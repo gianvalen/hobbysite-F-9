a863e35682449eb358f7d05f4cb11b987bb9ceac
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-03-16 17:10:52 +0800

    Added commission list and commission detail templates, and fixed settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commissions',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'hobbysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'commissions')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
