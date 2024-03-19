678d76735722aeeca8027411e492d9bf6b215bad
XaviJCF <xavier.fernando@student.ateneo.edu>
2024-03-19 22:58:09 +0800

    Placed online wiki templates in onlinewiki folder.

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -7,10 +7,10 @@ from .models import Article, ArticleCategory
 class ArticleList(ListView):
     model = ArticleCategory
     context_object_name = "articles"
-    template_name = "wiki_list.html"
+    template_name = "onlinewiki/wiki_list.html"
 
 
 class ArticleDetail(DetailView):
     model = Article
     context_object_name = "article"
-    template_name = "wiki_detail.html"
+    template_name = "onlinewiki/wiki_detail.html"
