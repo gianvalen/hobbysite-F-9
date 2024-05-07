7b4a18910b32ab71b24cf667086bc6a02051ecc6
Rafael <rafael.sese@student.ateneo.edu>
2024-05-07 17:18:49 +0800

    Set user as the initial author when creating article

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.urls import reverse_lazy, reverse
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
-from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
-from .models import Article, ArticleCategory, Comment
 from .forms import ArticleForm, CommentForm
+from .models import Article, ArticleCategory, Comment
 from user_management.models import Profile
 
 
@@ -26,13 +26,13 @@ class BlogDetailView(DetailView):
         ctx['comment_form'] = CommentForm()
         ctx['comments'] = Comment.objects.filter(article=self.object).order_by('-created_on')
         return ctx
-   
+
     def post(self, request, *args, **kwargs):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.author = Profile.objects.get(user_id=self.request.user)
-            comment.article = self.get_object() 
+            comment.author = Profile.objects.get(user=self.request.user)
+            comment.article = self.get_object()
             comment.entry = form.cleaned_data.get('entry')
             comment.save()
             return self.get(request, *args, **kwargs)
@@ -41,29 +41,35 @@ class BlogDetailView(DetailView):
             context = self.get_context_data(**kwargs)
             context["form"] = form
             return self.render_to_response(context)
-        
-        
+
+
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     form_class = ArticleForm
     template_name = "blog/blog_form.html"
-    success_url = reverse_lazy("blog:blog-list")
+
+    def get_initial(self):
+        author = Profile.objects.get(user=self.request.user)
+        return {'author': author}
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx["categories"] = ArticleCategory.objects.all()
         return ctx
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user.profile 
-        return super().form_valid(form)
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
     
+    def get_success_url(self):
+        return reverse('blog:blog-detail', kwargs={'pk': self.object.pk})
+
+
 class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     form_class = ArticleForm
     template_name = "blog/blog_form.html"
-    success_url = reverse_lazy("blog:blog-list")
+    success_url = reverse_lazy("blog:blog-detail")
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
@@ -71,5 +77,8 @@ class ArticleUpdateView(LoginRequiredMixin, UpdateView):
         return ctx
 
     def form_valid(self, form):
-        form.instance.author = self.request.user.profile 
+        form.instance.author = self.request.user.profile
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('blog:blog-detail', kwargs={'pk': self.object.pk})
