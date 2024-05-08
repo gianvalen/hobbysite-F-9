998bf96f49f280236996b279f778001b98b40d55
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-05-08 21:47:33 +0800

    Updated Files for Merging

--- hobbysite/profileapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import ProfileUpdate
-
-urlpatterns = [
-    path('', ProfileUpdate.as_view(), name = 'profile-update'),
-    path('accounts/', include("django.contrib.auth.urls"), name = 'profile-accounts')
-]
-
-app_name = 'profileapp'
