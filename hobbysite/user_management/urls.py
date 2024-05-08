5d08b33593e2d0f7bce2aad58ec313bdb971c17a
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-05-08 18:08:33 +0800

    Fixed urls and views for user_management

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 
-from .views import ProfileUpdateView
+from .views import ProfileUpdateView, dashboard
 
 urlpatterns = [
     path('', ProfileUpdateView.as_view(), name = 'profile-update'),
+    path('', dashboard, name = 'dashboard'),
 ]
 
 app_name = 'user_management'    
