4524e7a75f638bc3375d9401f98fe50632bc12fd
Jade <jade.ramos@student.ateneo.edu>
2024-05-08 18:17:49 +0800

    Modified the models accordingly

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import MerchListView, MerchDetailView
+from .views import MerchListView, MerchDetailView, MerchCreateView, MerchUpdateView, MerchCartView, MerchTransactionView
 
 urlpatterns = [
+    path('cart/', MerchCartView.as_view(), name='cart'),
+    path('transactions', MerchTransactionView.as_view(), name='transaction'),
     path('items/', MerchListView.as_view(), name='items'),
     path('item/<int:pk>/', MerchDetailView.as_view(), name='item'),
+    path('item/<int:pk>/edit', MerchUpdateView.as_view(), name='update'),
+    path('item/add', MerchCreateView.as_view(), name='add'),
 ]
 
 app_name = 'merchstore'
