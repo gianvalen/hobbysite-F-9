b09a8beee45b0c92971a87ccdb76aa5b86017daa
Jade <jade.ramos@student.ateneo.edu>
2024-05-08 18:14:59 +0800

    Modified fields and added Transaction Model

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('merchstore:items', args=[self.pk])
 
     class Meta:
         ordering = ['name']
@@ -15,8 +19,18 @@ class ProductType(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name="owner")
+    description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    STATUS_CHOICES = (
+        ('Available', 'Available'),
+        ('On sale', 'On sale'),
+        ('Out of Stock', 'Out of stock')
+    )
+
+    stock_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name="items")
 
     def __str__(self):
@@ -25,5 +39,29 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('merchstore:item', args=[self.pk])
 
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.stock_status = 'Out of Stock' 
+        elif self.stock > 0 and self.stock_status == 'Out of Stock':
+            self.stock_status = 'Available'   
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ['product_type', 'name']
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="buyer")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name="product")
+    amount = models.PositiveIntegerField()
+
+    TRANSACTION_CHOICES = (
+        ('On Cart', 'On Cart'),
+        ('To Pay', 'To Pay'),
+        ('To Ship', 'To Ship'),
+        ('To Receive', 'To Receive'),
+        ('Delivered', 'Delivered')        
+    )
+
+    transaction_status = models.CharField(max_length=20, choices=TRANSACTION_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
